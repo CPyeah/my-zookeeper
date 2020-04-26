b926b7064bd186514d0df673ce88a9d21ce00cb8
“CPyeah <chengpengyeah@126.com>
2020-04-26 09:27:54 +0800

    Initial commit

---
+++ src/main/java/org/cp/myzookeeper/MyZookeeperApplication.java
@@ -0,0 +1,13 @@
+package org.cp.myzookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyZookeeperApplication.class, args);
+    }
+
+}
